1123f09afe63954649a49852b4130f597fac225f
xiaohai1314-sys <1083328569@qq.com>
2025-07-10 17:57:12 +0800

    Update 厂长资源.js

--- js/厂长资源.js
+++ js/厂长资源.js
@@ -3,13 +3,11 @@ var rule = {
     host: 'https://www.czzy77.com',
     url: '/fyclassfyfilter',
     filterable: 1,
-    filter_url: '{{fl.area}}{{fl.class}}/page/fypage',
+    filter_url: '{{fl.cateId}}{{fl.class}}{{fl.area}}/page/fypage',
     filter: {
         "movie_bt": [
             {
-                "key": "area",
-                "name": "分类",
-                "value": [
+                "key": "area", "name": "分类", "value": [
                     {"v": "", "n": "全部"},
                     {"v": "/movie_bt_series/zhanchangtuijian", "n": "站长推荐"},
                     {"v": "/movie_bt_series/dyy", "n": "电影"},
@@ -19,7 +17,7 @@ var rule = {
                     {"v": "/movie_bt_series/mj", "n": "美剧"},
                     {"v": "/movie_bt_series/rj", "n": "日剧"},
                     {"v": "/movie_bt_series/hj", "n": "韩剧"},
-                    {"v": "/movie_bt_series/hwj", "n": "海外剧"},
+                    {"v": "/movie_bt_series/hwj", "n": "海外剧（其他）"},
                     {"v": "/movie_bt_series/huayudianying", "n": "华语电影"},
                     {"v": "/movie_bt_series/meiguodianying", "n": "欧美电影"},
                     {"v": "/movie_bt_series/ribendianying", "n": "日本电影"},
@@ -33,70 +31,196 @@ var rule = {
                 ]
             },
             {
-                "key": "class",
-                "name": "类型",
-                "value": [
-                    {"v": "", "n": "全部"},
-                    {"v": "/movie_bt_tags/chuanji", "n": "传记"},
-                    {"v": "/movie_bt_tags/etet", "n": "儿童"},
-                    {"v": "/movie_bt_tags/maoxian", "n": "冒险"},
-                    {"v": "/movie_bt_tags/juqing", "n": "剧情"},
-                    {"v": "/movie_bt_tags/dozuo", "n": "动作"},
-                    {"v": "/movie_bt_tags/doman", "n": "动漫"},
-                    {"v": "/movie_bt_tags/dhh", "n": "动画"},
-                    {"v": "/movie_bt_tags/lishi", "n": "历史"},
-                    {"v": "/movie_bt_tags/guzhuang", "n": "古装"},
-                    {"v": "/movie_bt_tags/tongxing", "n": "同性"},
-                    {"v": "/movie_bt_tags/xiju", "n": "喜剧"},
-                    {"v": "/movie_bt_tags/qihuan", "n": "奇幻"},
-                    {"v": "/movie_bt_tags/jiating", "n": "家庭"},
-                    {"v": "/movie_bt_tags/kubu", "n": "恐怖"},
-                    {"v": "/movie_bt_tags/xuanyi", "n": "悬疑"},
-                    {"v": "/movie_bt_tags/qingse", "n": "情色"},
-                    {"v": "/movie_bt_tags/kingsong", "n": "惊悚"},
-                    {"v": "/movie_bt_tags/zhanzhen", "n": "战争"},
-                    {"v": "/movie_bt_tags/gw", "n": "歌舞"},
-                    {"v": "/movie_bt_tags/wuxia", "n": "武侠"},
-                    {"v": "/movie_bt_tags/zainan", "n": "灾难"},
-                    {"v": "/movie_bt_tags/aiqing", "n": "爱情"},
-                    {"v": "/movie_bt_tags/fanzui", "n": "犯罪"},
-                    {"v": "/movie_bt_tags/dp", "n": "短片"},
-                    {"v": "/movie_bt_tags/kh", "n": "科幻"},
-                    {"v": "/movie_bt_tags/jlpp", "n": "纪录片"},
-                    {"v": "/movie_bt_tags/xb", "n": "西部"},
-                    {"v": "/movie_bt_tags/yd", "n": "运动"},
-                    {"v": "/movie_bt_tags/yy", "n": "音乐"}
+                "key": "class", "name": "类型", "value": [
+                    {"n": "全部", "v": ""},
+                    {"n": "传记", "v": "/movie_bt_tags/chuanji"},
+                    {"n": "儿童", "v": "/movie_bt_tags/etet"},
+                    {"n": "冒险", "v": "/movie_bt_tags/maoxian"},
+                    {"n": "剧情", "v": "/movie_bt_tags/juqing"},
+                    {"n": "动作", "v": "/movie_bt_tags/dozuo"},
+                    {"n": "动漫", "v": "/movie_bt_tags/doman"},
+                    {"n": "动画", "v": "/movie_bt_tags/dhh"},
+                    {"n": "历史", "v": "/movie_bt_tags/lishi"},
+                    {"n": "古装", "v": "/movie_bt_tags/guzhuang"},
+                    {"n": "同性", "v": "/movie_bt_tags/tongxing"},
+                    {"n": "喜剧", "v": "/movie_bt_tags/xiju"},
+                    {"n": "奇幻", "v": "/movie_bt_tags/qihuan"},
+                    {"n": "家庭", "v": "/movie_bt_tags/jiating"},
+                    {"n": "恐怖", "v": "/movie_bt_tags/kubu"},
+                    {"n": "悬疑", "v": "/movie_bt_tags/xuanyi"},
+                    {"n": "情色", "v": "/movie_bt_tags/qingse"},
+                    {"n": "惊悚", "v": "/movie_bt_tags/kingsong"},
+                    {"n": "战争", "v": "/movie_bt_tags/zhanzhen"},
+                    {"n": "歌舞", "v": "/movie_bt_tags/gw"},
+                    {"n": "武侠", "v": "/movie_bt_tags/wuxia"},
+                    {"n": "灾难", "v": "/movie_bt_tags/zainan"},
+                    {"n": "爱情", "v": "/movie_bt_tags/aiqing"},
+                    {"n": "犯罪", "v": "/movie_bt_tags/fanzui"},
+                    {"n": "短片", "v": "/movie_bt_tags/dp"},
+                    {"n": "科幻", "v": "/movie_bt_tags/kh"},
+                    {"n": "纪录片", "v": "/movie_bt_tags/jlpp"},
+                    {"n": "西部", "v": "/movie_bt_tags/xb"},
+                    {"n": "运动", "v": "/movie_bt_tags/yd"},
+                    {"n": "音乐", "v": "/movie_bt_tags/yy"}
                 ]
             }
         ]
     },
-    class_name: '全部&站长推荐&电影&电视剧&动画&国产剧&日剧&韩剧&美剧&海外剧&俄罗斯电影&加拿大电影&华语电影&印度电影&日本电影&欧美电影&法国电影&英国电影&韩国电影&纪录片',
-    class_url: 'movie_bt&/movie_bt_series/zhanchangtuijian&/movie_bt_series/dyy&/movie_bt_series/dianshiju&/movie_bt_series/dohua&/movie_bt_series/guochanju&/movie_bt_series/rj&/movie_bt_series/hj&/movie_bt_series/mj&/movie_bt_series/hwj&/movie_bt_series/eluosidianying&/movie_bt_series/jianadadianying&/movie_bt_series/huayudianying&/movie_bt_series/yindudianying&/movie_bt_series/ribendianying&/movie_bt_series/meiguodianying&/movie_bt_series/faguodianying&/movie_bt_series/yingguodianying&/movie_bt_series/hanguodianying&/movie_bt_tags/jlpp',
-    headers: {
-        'User-Agent': 'MOBILE_UA',
-        'Cookie': 'esc_search_captcha=1'
-    },
     searchUrl: '/page/fypage?s=**',
     searchable: 2,
+    filterable: 0,
+    headers: {
+        'User-Agent': 'MOBILE_UA',
+        'Cookie': getItem('cookie') || 'esc_search_captcha=1'
+    },
+    class_name: '全部&豆瓣电影Top250&高分影视&最新电影&热映中&站长推荐&电影&电视剧&动画&国产剧&日剧&韩剧&美剧&海外剧&俄罗斯电影&加拿大电影&华语电影&印度电影&日本电影&欧美电影&法国电影&英国电影&韩国电影&纪录片',
+    class_url: 'movie_bt&dbtop250&gaofenyingshi&zuixindianying&reyingzhong&/movie_bt_series/zhanchangtuijian&/movie_bt_series/dyy&/movie_bt_series/dianshiju&/movie_bt_series/dohua&/movie_bt_series/guochanju&/movie_bt_series/rj&/movie_bt_series/hj&/movie_bt_series/mj&/movie_bt_series/hwj&/movie_bt_series/eluosidianying&/movie_bt_series/jianadadianying&/movie_bt_series/huayudianying&/movie_bt_series/yindudianying&/movie_bt_series/ribendianying&/movie_bt_series/meiguodianying&/movie_bt_series/faguodianying&/movie_bt_series/yingguodianying&/movie_bt_series/hanguodianying&movie_bt//movie_bt_tags/jlpp',
+    play_parse: true,
+    lazy: `js:
+        pdfh = jsp.pdfh;
+        var html = request(input);
+        var ohtml = pdfh(html, '.videoplay&&Html');
+        var url = pdfh(ohtml, "body&&iframe&&src");
+        if (/Cloud/.test(url)) {
+            var ifrwy = request(url);
+            let code = ifrwy.match(/var url = '(.*?)'/)[1].split('').reverse().join('');
+            let temp = '';
+            for (let i = 0x0; i < code.length; i = i + 0x2) {
+                temp += String.fromCharCode(parseInt(code[i] + code[i + 0x1], 0x10))
+            }
+            input = {
+                jx: 0,
+                url: temp.substring(0x0, (temp.length - 0x7) / 0x2) + temp.substring((temp.length - 0x7) / 0x2 + 0x7),
+                parse: 0
+            }
+        } else if (/decrypted/.test(ohtml)) {
+            var phtml = pdfh(ohtml, "body&&script:not([src])&&Html");
+            eval(getCryptoJS());
+            var scrpt = phtml.match(/var.*?\\)\\);/g)[0];
+            var data = [];
+            eval(scrpt.replace(/md5/g, 'CryptoJS').replace('eval', 'data = '));
+            input = {
+                jx: 0,
+                url: data.match(/url:.*?[\\'\\"](.*?)[\\'\\"]/)[1],
+                parse: 0
+            }
+        } else {
+            input
+        }
+    `,
     推荐: '.bt_img;ul&&li;*;*;*;*',
     double: true,
-    一级: '.bt_img&&ul&&li;h3.dytit&&Text;img.lazy&&data-original;.jidi&&Text;a&&href',
+    一级: '.bt_img&&ul&&li;h3.dytit&&Text;img.lazy&&data-original;js:pdfh(html,\'.jidi&&Text\')||pdfh(html,\'.dy_remarks&&Text\')||\'\';a&&href',
     二级: {
         "title": "h1&&Text;.moviedteail_list li&&a&&Text",
         "img": "div.dyimg img&&src",
-        "desc": ".moviedteail_list li:eq(3) a&&Text;.moviedteail_list li:eq(2) a&&Text;.moviedteail_list li:eq(1) a&&Text;.moviedteail_list li:eq(7)&&Text;.moviedteail_list li:eq(5)&&Text",
+        "desc": ".moviedteail_list li:eq(3) a&&Text;js:pdfh(html,'.moviedteail_list li:eq(5) a&&Text')||pdfh(html,'.moviedteail_list li:eq(5)&&Text');.moviedteail_list li:eq(1) a&&Text;.moviedteail_list li:eq(7)&&Text;.moviedteail_list li:eq(5)&&Text",
         "content": ".yp_context&&Text",
         "tabs": ".mi_paly_box span",
         "lists": ".paly_list_btn:eq(#id) a"
     },
-    play_parse: true,
-    lazy: `js:
-        var html = request(input);
-        var url = pdfh(html, "iframe&&src");
-        if (!url) {
-            url = html.match(/<iframe[^>]+src=["']([^"']+)["']/)[1];
+    搜索: '.search_list&&ul&&li;*;*;*;*',
+    
+    // 登录功能
+    login: {
+        enable: true,
+        key: "登录",
+        action: `js:
+            let loginUrl = HOST + '/login';
+            let html = request(loginUrl);
+            let token = pdfh(html, 'input[name="__token__"]&&value');
+            
+            if (!token) {
+                throw '获取登录token失败';
+            }
+            
+            // 构建登录请求数据
+            let data = {
+                'account': USERNAME,
+                'password': PASSWORD,
+                '__token__': token
+            };
+            
+            // 提交登录请求
+            let res = request(loginUrl, {
+                method: 'POST',
+                data: data,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Referer': loginUrl,
+                    'User-Agent': 'MOBILE_UA'
+                }
+            });
+            
+            // 检查登录是否成功
+            if (res.body.indexOf('登录成功') > -1 || res.body.indexOf('我的会员') > -1) {
+                // 获取并保存Cookie
+                let cookie = res.headers['Set-Cookie'] || res.headers['set-cookie'];
+                if (cookie) {
+                    setItem('cookie', cookie);
+                    rule.headers.Cookie = cookie;
+                    '登录成功';
+                } else {
+                    '登录成功但未获取到Cookie';
+                }
+            } else if (res.body.indexOf('验证码') > -1) {
+                // 处理需要验证码的情况
+                let captchaUrl = pdfh(html, 'img.captcha&&src');
+                if (captchaUrl) {
+                    let captcha = captcha(captchaUrl);
+                    data.verify = captcha;
+                    
+                    // 重新提交带验证码的请求
+                    res = request(loginUrl, {
+                        method: 'POST',
+                        data: data,
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded',
+                            'Referer': loginUrl,
+                            'User-Agent': 'MOBILE_UA'
+                        }
+                    });
+                    
+                    if (res.body.indexOf('登录成功') > -1) {
+                        let cookie = res.headers['Set-Cookie'] || res.headers['set-cookie'];
+                        if (cookie) {
+                            setItem('cookie', cookie);
+                            rule.headers.Cookie = cookie;
+                            '验证码登录成功';
+                        } else {
+                            '验证码登录成功但未获取到Cookie';
+                        }
+                    } else {
+                        '验证码登录失败：' + pdfh(res.body, 'div.error&&Text');
+                    }
+                } else {
+                    '需要验证码但未找到验证码图片';
+                }
+            } else {
+                '登录失败：' + pdfh(res.body, 'div.error&&Text');
+            }
+        `
+    },
+    
+    // 初始化检查登录状态
+    init: `js:
+        // 检查Cookie是否有效
+        let testUrl = HOST + '/user';
+        let res = request(testUrl, {headers: rule.headers});
+        if (res.body.indexOf('请登录') > -1 || res.body.indexOf('登录') > -1) {
+            // Cookie失效，清除存储
+            setItem('cookie', null);
+            rule.headers.Cookie = 'esc_search_captcha=1';
+            'Cookie已失效，请重新登录';
+        } else {
+            'Cookie有效';
         }
-        input = { jx: 0, url: url, parse: 0 }
     `,
-    搜索: '.search_list&&ul&&li;h3&&Text;img.lazy&&data-original;.jidi&&Text;a&&href'
+    
+    // 退出登录功能
+    logout: `js:
+        setItem('cookie', null);
+        rule.headers.Cookie = 'esc_search_captcha=1';
+        '已退出登录';
+    `
 };
